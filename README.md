43d96619ee1f5e78f5715b333a90eca3bfdeea22
gregzuro <greg@zuro.net>
2017-11-02 19:53:11 -0700

    initial

---
+++ README.md
@@ -0,0 +1,29 @@
+# mqtt-client
+
+## Description
+
+This runs on a gateway, subscribes to certain MQTT topics on localhost, then sends the received data to Sixgill Sense 2.0 via their ingress API.
+
+## Building
+
+- clone repository
+- `go get`
+- `GOOS=linux go build`
+- scp the resulting binary to the target machine along with the `demo` helper script
+- modify the sixgill Ingress API key and any other parameters in `demo` if necessary
+
+## Usage
+
+From the gatewaty, either 
+
+`./demo`
+
+or
+
+`./mqtt-client <flags>`
+
+if you need more control.
+
+Do `./mqtt-client -help' or `./demo -help` for help on the flags.
+
+
---
+++ main.go
@@ -0,0 +1,212 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"gopkg.in/resty.v1"
+
+	"github.com/rs/xid"
+	pb "github.com/sixgill/sense-ingress-api/proto"
+
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+)
+
+const jwtFile string = "./jwt"
+
+var jwt string
+
+// default mqtt message handler
+var f MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
+
+	// send payload to sixgill Ingress API server
+	statusCode, err := PostEvent(*sixgillIngressAddress+"/v1/iot/events", jwt, msg.Payload())
+	if err != nil {
+		log.Println(err.Error())
+	}
+	log.Printf("TOPIC: %s MSG: %s STATUSCODE: %d\n", msg.Topic(), msg.Payload(), statusCode)
+
+}
+
+var sixgillIngressAddress *string
+
+func main() {
+
+	var err error
+
+	// get flags
+	mqttBrokerAddress := flag.String("mqtt-broker-address", "localhost", "IP address of the MQTT broker")
+	mqttBrokerPort := flag.String("mqtt-broker-port", "1883", "broker's port")
+	mqttTopic := flag.String("mqtt-topic", "", "MQTT topic")
+	sixgillIngressAddress = flag.String("sixgill-ingress-address", "", "IP address of the Sixgill Ingress API server")
+	sixgillIngressAPIKey := flag.String("sixgill-ingress-api-key", "", "API key for Sixgill Ingress API server")
+	forceRegister := flag.Bool("force-register", false, "force registration (to update a bad JWT, for instance)")
+	flag.Parse()
+
+	// if no topic specified
+	if len(*mqttTopic) == 0 {
+		log.Println("no mqtt topic specified (-mqtt-topic)")
+		os.Exit(1)
+	}
+
+	// if no sixgill-ingress-address specified
+	if len(*sixgillIngressAddress) == 0 {
+		log.Println("no Sixgill Ingress API server address specified (-sixgill-ingress-address)")
+		os.Exit(1)
+	}
+
+	// if no sixgill-ingress-api-key specified
+	if len(*sixgillIngressAPIKey) == 0 {
+		log.Println("no Sixgill Ingress API key specified (-sixgill-ingress-api-key)")
+		os.Exit(1)
+	}
+
+	// fetch any existing JWT
+	jwt, err = GetJwtFromFile()
+	if *forceRegister || err != nil {
+		log.Println("doing registration (no jwt file present or -force-register specified)")
+		// do registration
+		url := *sixgillIngressAddress + "/v1/registration"
+		statusCode, registrationResponse, err := DoRegistration(url, *sixgillIngressAPIKey)
+		if err != nil {
+			log.Println("unable to do registration (with error): " + err.Error())
+			os.Exit(1)
+		}
+		if statusCode != 200 {
+			log.Println("unable to do registration (with status code): ", statusCode)
+			os.Exit(1)
+		}
+
+		jwt = registrationResponse.Token
+
+		// save JWT for next time
+		err = PutJwtToFile(jwt)
+		if err != nil {
+			log.Println("unable to write JWT: " + err.Error())
+		}
+
+	} else {
+		log.Println("got jwt from file")
+	}
+
+	log.Println("setting up mqtt broker")
+	// set up mqtt broker for specified address and port
+	opts := MQTT.NewClientOptions().AddBroker("tcp://" + *mqttBrokerAddress + ":" + *mqttBrokerPort)
+
+	// set a unique mqtt client id (duplicates will stall all parties with that id)
+	opts.SetClientID("mqtt-client-" + xid.New().String())
+	opts.SetDefaultPublishHandler(f)
+
+	// create and start the client
+	c := MQTT.NewClient(opts)
+	if token := c.Connect(); token.Wait() && token.Error() != nil {
+		log.Println("unable to do start mqtt client:", token.Error())
+		os.Exit(1)
+	}
+
+	// subscribe to the mqtt topic
+	// TODO: subscribe to multiple topics
+	token := c.Subscribe(*mqttTopic, 0, nil)
+	token.Wait()
+	if token.Error() != nil {
+		log.Println("unable to subscribe to topic '"+*mqttTopic+"':", token.Error())
+		os.Exit(1)
+	}
+
+	// recognize stop-related signals
+	exitSignal := make(chan os.Signal)
+	signal.Notify(exitSignal, syscall.SIGINT, syscall.SIGTERM)
+
+	// wait / block on those signals
+	<-exitSignal
+
+	// indicate successful cleanup
+	log.Println("bye")
+
+	// unsubscribe from mqtt topic
+	if token := c.Unsubscribe(*mqttTopic); token.Wait() && token.Error() != nil {
+		log.Println("unable to unsubscribe from topic '"+*mqttTopic+"'", token.Error())
+		os.Exit(1)
+	}
+
+	// some mqtt-related delay
+	c.Disconnect(250)
+
+	// free and clear!
+}
+
+// DoRegistration registers this application with the sixgill api server
+func DoRegistration(url, apiKey string) (int, pb.RegistrationResponse, error) {
+
+	// curl -X POST "http://sense-ingress-api-staging.sixgill.run/v1/registration"  -d '{
+	// 	"apiKey":"01BWHNJHFCZXDVDYPTK8080WC1",
+	// 	"properties":{
+	// 	  "timestamp":1509038692,
+	// 	  "manufacturer":"Apple",
+	// 	  "model":"advantech",
+	// 	  "os":"wrlinux",
+	// 	  "osVersion":"7.0.0.13",
+	// 	  "softwareVersion":"sixgill-mqtt-v0.1",
+	// 	  "type":"wrlinux",
+	// 	  "sensors":["temperature","humidity"]
+	//   }
+	// }'
+
+	request := &pb.RegistrationRequest{
+		ApiKey: apiKey,
+		Properties: &pb.Property{
+			Timestamp:       int64(time.Now().UTC().Second()),
+			Manufacturer:    "Intel",
+			Model:           "Advantech",
+			Os:              "wrlinux",
+			OsVersion:       "7.0.0.13",
+			SoftwareVersion: "sixgill-mqtt-v0.1",
+			Type:            "wrlinux",
+			Sensors:         []string{"temperature", "humidity"},
+		},
+	}
+
+	response := &pb.RegistrationResponse{}
+	resp, err := resty.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(request).
+		SetResult(response).
+		SetContentLength(true).
+		Post(url)
+
+	return resp.StatusCode(), *response, err
+}
+
+// GetJwtFromFile gets the previously stored JWT from the file
+func GetJwtFromFile() (string, error) {
+	jwt, err := ioutil.ReadFile(jwtFile)
+	log.Println("read from jwt file")
+	return string(jwt), err
+}
+
+// PutJwtToFile puts the JWT into the file
+func PutJwtToFile(jwt string) error {
+	log.Println("writing to jwt file")
+	return ioutil.WriteFile(jwtFile, []byte(jwt), 0644)
+}
+
+// PostEvent POSTs the event to the ingress API server using the jwt
+func PostEvent(url, jwt string, event []byte) (int, error) {
+
+	request := `{"data":{` + string(event) + `}}`
+	response := &pb.RegistrationResponse{}
+	resp, err := resty.R().
+		SetHeader("Content-Type", "application/json").
+		SetAuthToken(jwt).
+		SetBody(request).
+		SetResult(response).
+		SetContentLength(true).
+		Post(url)
+
+	return resp.StatusCode(), err
+}
